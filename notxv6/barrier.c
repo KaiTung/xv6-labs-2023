070bf1bd5fe62c7d11c5aa8b1799a6ce61fa22c7
KaiTung <allen01105@gmail.com>
2024-11-25 20:38:34 +0800

    finish lab thread

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -26,11 +26,19 @@ static void
 barrier()
 {
   // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  // go to sleep on cond, releasing lock mutex, acquiring upon wake up
+  if(bstate.nthread < nthread){
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+
+  if(bstate.nthread == nthread){
+    bstate.nthread = 0;
+    bstate.round++;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,35 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// Saved thread registers for kernel context switches.
+struct thread_context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+
+  // modifying struct thread to hold registers is a good plan
+  struct thread_context thread_context;
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
@@ -60,6 +84,9 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+
+    thread_switch((uint64)&t->thread_context,(uint64)&current_thread->thread_context);
+
   } else
     next_thread = 0;
 }
@@ -74,6 +101,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->thread_context.ra = (uint64)func;
+  t->thread_context.sp = (uint64)(t->stack + STACK_SIZE);
 }
 
 void 
